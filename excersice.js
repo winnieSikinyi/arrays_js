4ac62e1ec67c4b03b11aacd5a1743e1d6335bed4
winnieSikinyi <winniesikinyi@gmail.com>
2023-03-18 16:12:30 +0300

    arr

--- excersice.js
+++ excersice.js
@@ -1,33 +1,37 @@
 // Qn1 Find the last element of the following arrays.
-arr1 = [3,7,34,90,12]
-arr2 = [true, "green", "where",12,56]
+// arr1 = [3,7,34,90,12]
+// arr2 = [true, "green", "where",12,56]
 
 let arr1 = [3,7,34,90,12];
-function Gfg(){
-    let l = arr1.slice(-1);
-    console.log(arr1);
-}
-let arr2 = [true,"green","where",12,56]
-function Gfg(){
-    let l = arr1.slice(-1);
-    console.log(arr2);
-}
+console.log(arr1.slice(-1))
+let arr2 = [true, "green", "where",12,56]
+console.log(arr2.slice(-1))
+
+// console.log(arr1);
+// console.log(arr2);
+// arr1.slice(-1);
+// arr2.pop(-1)
 
 
 //Write a JS program that will join the following array elements into a string
-myPets = ["Cow", "Python", "Snake", "Dog"];
-let text = myPets.toString();
-console.log(myPets);
+let myPets = ["Cow", "Python", "Snake", "Dog"];
+console.log(myPets.join(` `));
 
 //Write a JS script to sort the following array items
 var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+let sortedArr = arr3.sort();
+console.log(sortedArr);
 
 //Write a program to remove duplicates from the following array.
 //Console the array without duplicates, and console another array that only contains the duplicates
-var arr = ["apple", "mango", "apple",
+let arr = ["apple", "mango", "apple",
             "orange", "mango", "mango"];
+             console.log(...new Set(arr));
+
 // Write a JS script to search for the following word in the array.
 //If the word is present, console it else console "the search word was not found"
 let arr5 = ["the", "way", "x", 4, 23];
+console.log(arr5.includes(4))
 //Write a JS script to sort the following string
-let word = "lufituaeb"
+let word = ["lufituaeb"].sort();
+console.log(word);
